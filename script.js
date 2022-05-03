f2ce60a462818f567658bba375d480bce9f743e2
ObjectifGuitare <sebastien@DESKTOP-5GSLS9M.localdomain>
2022-05-03 15:06:03 +0200

    fluidified le mouvement du paddle

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let boardBottomX = 300;
+
 let paddleRight = {
     x: document.querySelector(".Rdroite").getBoundingClientRect().left,
     y: document.querySelector(".Rdroite").getBoundingClientRect().top
@@ -8,21 +10,32 @@ let paddleLeft = {
     y: document.querySelector(".Rgauche").getBoundingClientRect().top
 }
 
-function startGame(e)
+function movePaddle(e)
 {
-
     document.querySelector(".Rgauche").style.position = 'absolute';
     if (document.querySelector(".Rgauche").getBoundingClientRect().top > document.querySelector(".board").getBoundingClientRect().top + 5 &&
         e.code === "ArrowUp")
     {
-        document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top - 5}px`
+        document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top - 15}px`
     }
 
-    if ( document.querySelector(".Rgauche").getBoundingClientRect().top > document.querySelector(".board").getBoundingClientRect().top - 11 &&
+    else if ( document.querySelector(".Rgauche").getBoundingClientRect().top < document.querySelector(".board").getBoundingClientRect().top + boardBottomX &&
         e.code === "ArrowDown")
     {
-        document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top + 5}px`
+        document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top + 15}px`
     }
     
 }
-document.body.addEventListener("keydown", startGame);
+
+function stopPaddle()
+{
+
+}
+
+// document.body.addEventListener("keydown", movePaddle)
+
+document.body.addEventListener("keydown", (e) =>{
+    let interval = setInterval(() => {movePaddle(e)}, Math.ceil(1000/60));
+    document.body.addEventListener("keyup", () =>{clearInterval(interval)});
+    document.body.addEventListener("keydown", () =>{clearInterval(interval)})
+});
