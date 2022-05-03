b6911ec8c4ec171692d345819d39d92e75829afb
ObjectifGuitare <sebastien@DESKTOP-5GSLS9M.localdomain>
2022-05-03 16:52:24 +0200

    ball is moving

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let boardBottomX = 300;
+let boardBottomY = 300;
+let boardRightX = 500;
+let begin = 1;
 
 let paddleRight = {
     x: document.querySelector(".Rdroite").getBoundingClientRect().left,
@@ -19,7 +21,7 @@ function movePaddle(e)
         document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top - 15}px`
     }
 
-    else if ( document.querySelector(".Rgauche").getBoundingClientRect().top < document.querySelector(".board").getBoundingClientRect().top + boardBottomX &&
+    else if ( document.querySelector(".Rgauche").getBoundingClientRect().top < document.querySelector(".board").getBoundingClientRect().top + boardBottomY &&
         e.code === "ArrowDown")
     {
         document.querySelector(".Rgauche").style.top = `${document.querySelector(".Rgauche").getBoundingClientRect().top + 15}px`
@@ -27,15 +29,30 @@ function movePaddle(e)
     
 }
 
-function stopPaddle()
-{
-
-}
-
-// document.body.addEventListener("keydown", movePaddle)
-
+//fluid way to move the paddle 60fps no scope 360
 document.body.addEventListener("keydown", (e) =>{
-    let interval = setInterval(() => {movePaddle(e)}, Math.ceil(1000/60));
-    document.body.addEventListener("keyup", () =>{clearInterval(interval)});
-    document.body.addEventListener("keydown", () =>{clearInterval(interval)})
+    let startPaddleMove = setInterval(() => {movePaddle(e)}, Math.ceil(1000/60));
+    document.body.addEventListener("keyup", () =>{clearInterval(startPaddleMove)});
+    document.body.addEventListener("keydown", () =>{clearInterval(startPaddleMove)})
+
 });
+
+
+function moveBall()
+{
+    if (begin)
+    {
+        document.querySelector(".ball").style.top = `${document.querySelector(".ball").getBoundingClientRect().top + 1}px`;
+        document.querySelector(".ball").style.left = `${document.querySelector(".ball").getBoundingClientRect().left + 1}px`;
+        console.log("bonjour")
+    }
+    if (document.querySelector(".ball").getBoundingClientRect().top < document.querySelector(".board").getBoundingClientRect().top + 480)
+    {
+        begin = 0;
+        document.querySelector(".ball").style.top = `${document.querySelector(".ball").getBoundingClientRect().top - 1}px`;
+        document.querySelector(".ball").style.left = `${document.querySelector(".ball").getBoundingClientRect().left - 1}px`;
+        console.log("aurevoir")
+    }
+    
+}
+let startBallMove = setInterval(() =>{moveBall(begin)}, Math.ceil(1000/60));
